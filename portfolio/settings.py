f205f45aba7c4b7bbfc9e8f19c582d94c8e58c44
Brian <semperindoctrina19@gmail.com>
2021-05-30 03:35:00 -0500

    removed SSL from email settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -160,12 +160,11 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_PORT = 587
 
 TAGGIT_CASE_INSENSITIVE = True
 
