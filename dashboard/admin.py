234e2d6cc11b47141b163a0edbe578f1748397a5
Brian <semperindoctrina19@gmail.com>
2021-11-29 20:53:12 -0600

    added profile model to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Service, Project, Update
+from .models import Service, Project, Update, Profile
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
@@ -11,4 +11,8 @@ class ServiceAdmin(admin.ModelAdmin):
 
 @admin.register(Update)
 class UpdateAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
     pass
