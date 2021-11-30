db6ada79f11b6f17e8993f94f8fc7eb0ee4aebff
Brian <semperindoctrina19@gmail.com>
2021-11-29 20:50:11 -0600

    removed bio and location field from Profile model, updated the dashboard template

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
-from django.urls import reverse
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from imagekit.models import ProcessedImageField
@@ -18,8 +16,6 @@ class Profile(models.Model):
         options={'quality':85}
     )
     display_name = models.CharField(max_length=100)
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return f"{self.display_name}'s Profile"
