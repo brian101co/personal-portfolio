c28543ba4e30549c6cf666c7ad73edf48f8499c5
Brian <semperindoctrina19@gmail.com>
2021-12-05 00:30:04 -0600

    added feature boolean field to review model

--- review/models.py
+++ review/models.py
@@ -6,7 +6,8 @@ class Review(models.Model):
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name="reviews")
     reviewer_photo = models.ImageField(upload_to="images/", blank=True)
     reviewer_name = models.CharField(max_length=120)
-    review_body = models.TextField()  
+    review_body = models.TextField()
+    featured = models.BooleanField(default=False)  
 
     def __str__(self):
         return self.reviewer_name
